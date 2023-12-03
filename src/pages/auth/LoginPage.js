10b4dc7150dab49f4838c0cf04e3d92042bb213f
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-03 13:48:30 +0100

    enable login button on change

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,7 +1,10 @@
 //Simport { useState } from "react";
+import { useState } from "react";
 import Button from"../../components/shared/Button";
 import { login } from "./service";
 function LoginPage({onLogin}){
+    const[email,setEmail]= useState('');
+    const[password,setPassword]=useState('');
     
     const handleSubmit=async(event) =>{
         event.preventDefault();
@@ -10,13 +13,26 @@ function LoginPage({onLogin}){
         onLogin();
         //setIsLogged(true);
     };
+    const handleEmailChange=(event)=>{
+        setEmail(event.target.value)
+ //console.log(event.target.value);
+
+    };
+
+    const handlePasswordChange=(event)=>{
+        setPassword(event.target.value)
+       // console.log(event.target.value);
+       
+           };
+           const disabled=!(email&& password);
+    
     return<div>
         <h1>Loginto Walla-react</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="email"></input>
-            <input type="password" name="password"></input>
+            <input type="text" name="email"onChange={handleEmailChange}></input>
+            <input type="password" name="password" onChange={handlePasswordChange}></input>
             
-            <Button type="submit" $variant="primary" disabled={false}>Log in</Button>
+            <Button type="submit" $variant="primary" disabled={disabled}>Log in</Button>
 
         </form>
     </div>;
