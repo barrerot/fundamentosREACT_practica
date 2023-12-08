05408a5ed4c123243a66d3b1573baf94ff0df255
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-09 00:07:28 +0100

    function create advert added

--- src/pages/adverts/service.js
+++ src/pages/adverts/service.js
@@ -5,7 +5,10 @@ const advertsUrl = '/api/v1/adverts';
 export const getLatestAdverts = () => {
   return client.get(advertsUrl);
 };
-
+export const createAdvert=(advert) => {
+const url=advertsUrl
+return client.post(url,advert);
+};
 
 
 
