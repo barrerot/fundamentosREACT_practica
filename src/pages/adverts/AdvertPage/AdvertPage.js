7ac81b9100984d1acea72194f99490a5ab671f52
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-09 01:04:32 +0100

    initial version of advert page datail

--- src/pages/adverts/AdvertPage/AdvertPage.js
+++ src/pages/adverts/AdvertPage/AdvertPage.js
@@ -1,9 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Content from "../../../components/shared/layout/Content";
+import { useEffect, useState } from "react";
+import{getAdvert}from "../service";
 function AdvertPage(){
     const params =useParams();
+    const navigate=useNavigate();
+    const[advert,setAdvert]=useState(null)
+    useEffect(() =>{
+        getAdvert(params.advertId).then(advert=>setAdvert(advert)).catch(error=>{
+            if(error.status==404){
+                navigate("/404");
+            }
+        });
+    },[]);
     return<Content title="Advert Detail">
-        <div>Advert Detail{params.advertId} goes here..</div>
+        <div>Advert Detail{params.advertId} goes here..
+        {advert&& <code>{JSON.stringify(advert)}</code>}
+        </div>
     </Content>
 }
 export default AdvertPage;
--- src/pages/adverts/service.js
+++ src/pages/adverts/service.js
@@ -5,10 +5,7 @@ const advertsUrl = '/api/v1/adverts';
 export const getLatestAdverts = () => {
   return client.get(advertsUrl);
 };
-export const createAdvert=(advert) => {
-const url=advertsUrl
-return client.post(url,advert);
+export const getAdvert = advertId => {
+  const url = `${advertsUrl}/${advertId}`;
+  return client.get(url);
 };
-
-
-
