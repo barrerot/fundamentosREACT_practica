e0aba5f76eeae21d21f84f89af5b6411018922b2
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-09 16:19:30 +0100

    detail advert modified

--- src/pages/adverts/AdvertPage/AdvertPage.js
+++ src/pages/adverts/AdvertPage/AdvertPage.js
@@ -14,7 +14,7 @@ function AdvertPage(){
         });
     },[navigate,params.advertId]);
     return<Content title="Advert Detail">
-        <div>Advert Detail{params.advertId} goes here..
+        <div>
         {advert&& <div>{/*JSON.stringify(advert)*/}<img src={advert.photo}width={150} height={200}></img><br/><b>Nombre del articulo:</b><h2>{advert.name}</h2><b>Precio del articulo:</b>{advert.price}€<br/><b>tipo:</b>{advert.sale ? "es de venta" : "es de compra"}<br/><b>tags</b>
         {advert.tags.map((tag, index) => (
                   <span key={index} className='tag-span'> {tag} </span>
