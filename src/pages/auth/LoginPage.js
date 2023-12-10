a38f235b9d043fcfa5206e493180ca7516b24cd9
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-10 11:37:35 +0100

    rememberme check included

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,69 +1,81 @@
-
 import { useContext, useState } from "react";
-import Button from"../../components/shared/Button";
+import Button from "../../components/shared/Button";
 import { login } from "./service";
 import { AuthContext, useAuth } from "./context";
 import './LoginPage.css';
 import { useLocation, useNavigate } from "react-router-dom";
 
-function LoginPage(){
-    const{onLogin}=useAuth();
-    
-    const[credentials, setCredentials]=useState({
-        email:'',password:'',
-    })
-    const[error,setError] =useState(null);
-    const[isFetching, setIsFetching]=useState(false);
-    const location =useLocation();
-    const navigate=useNavigate();
-    
-    const handleSubmit=async(event) =>{
+function LoginPage() {
+    const { onLogin } = useAuth();
+
+    const [credentials, setCredentials] = useState({ email: '', password: '' });
+    const [rememberMe, setRememberMe] = useState(false);
+    const [error, setError] = useState(null);
+    const [isFetching, setIsFetching] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleRememberMeChange = (event) => {
+        setRememberMe(event.target.checked);
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         try {
             setIsFetching(true);
-            await login(credentials);
+            await login(credentials, rememberMe);
             onLogin();
             setIsFetching(false);
-            const to=location?.state?.from||'/';
-            navigate(to,{replace:true});
+            const to = location?.state?.from || '/';
+            navigate(to, { replace: true });
         } catch (error) {
             setIsFetching(false);
             setError(error);
-            
         }
-    
-        
-        
-       
-        
-    };
-    const handleEmailChange=(event)=>{
-        setCredentials({email:event.target.value,password: credentials.password});
- 
-
     };
 
-    const handlePasswordChange=(event)=>{
-        setCredentials({email: credentials.email, password:event.target.value});
-       
-       
-           };
-           const resetError=() =>{
-            setError(null);
-           }
-           const disabled=!(credentials.email&& credentials.password)||isFetching;
-    
-    return<div>
-        <h1>Loginto Walla-react</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="email"onChange={handleEmailChange}></input>
-            <input type="password" name="password" onChange={handlePasswordChange}></input>
-            
-            <Button type="submit" $variant="primary" disabled={disabled}>{isFetching?"Connecting..." :"Login"}</Button>
-            
-            {error&&<div className="loginPage-error" onClick={resetError}>{error.message}</div>}
+    const handleEmailChange = (event) => {
+        setCredentials({ ...credentials, email: event.target.value });
+    };
 
-        </form>
-    </div>;
+    const handlePasswordChange = (event) => {
+        setCredentials({ ...credentials, password: event.target.value });
+    };
+
+    const resetError = () => {
+        setError(null);
+    };
+
+    const disabled = !(credentials.email && credentials.password) || isFetching;
+
+    return (
+        <div>
+            <h1>Login to Walla-react</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="email" onChange={handleEmailChange} />
+                <input type="password" name="password" onChange={handlePasswordChange} />
+
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={rememberMe}
+                        onChange={handleRememberMeChange}
+                    />
+                    Recordar sesión
+                </label>
+
+                <Button type="submit" $variant="primary" disabled={disabled}>
+                    {isFetching ? "Connecting..." : "Login"}
+                </Button>
+
+                {error && (
+                    <div className="loginPage-error" onClick={resetError}>
+                        {error.message}
+                    </div>
+                )}
+            </form>
+        </div>
+    );
 }
+
 export default LoginPage;
--- src/pages/auth/service.js
+++ src/pages/auth/service.js
@@ -1,16 +1,22 @@
-import client, { removeAuthorizationHeader, setAuthorizationHeader } from"../../api/client";
+import client, { removeAuthorizationHeader, setAuthorizationHeader } from "../../api/client";
 import storaje from "../../utils/storaje";
-export const login=(credentials) =>{
-return client.post('/api/auth/login', credentials)
-.then(({accessToken})=>{
-    {setAuthorizationHeader(accessToken)
-    storaje.set('auth',accessToken);
-    };
-});
+
+export const login = (credentials, rememberMe) => {
+    return client.post('/api/auth/login', credentials)
+        .then(({ accessToken }) => {
+            setAuthorizationHeader(accessToken);
+            if (rememberMe) {
+                storaje.set('auth', accessToken); // Guardar en localStorage
+            } else {
+                sessionStorage.setItem('auth', accessToken); // Guardar en sessionStorage
+            }
+        });
 };
-export const logout =()=> {
-    return Promise.resolve().then(()=>{
+
+export const logout = () => {
+    return Promise.resolve().then(() => {
         removeAuthorizationHeader();
-        storaje.remove('auth');
+        storaje.remove('auth'); // Limpiar localStorage
+        sessionStorage.removeItem('auth'); // Limpiar sessionStorage
     });
 };
